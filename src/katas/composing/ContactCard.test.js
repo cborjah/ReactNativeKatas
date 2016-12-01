d902f5f623a74a3e273e9df781075dd9f3159e34
Christian Borja <cpborja32@gmail.com>
2016-11-30 18:44:07 -0800

    ContactCard Completed

--- src/katas/composing/ContactCard.test.js
+++ src/katas/composing/ContactCard.test.js
@@ -26,17 +26,17 @@ import colors from '@/runner/colors'
 
 const ContactCard= (props)=>{
   return (
-    <View style={[styles.container, {alignItems:'stretch'}]}>
-      <View style={{}}>
+    <View style={[styles.container, {alignItems: 'stretch'}]}>
+      <View style={{margin:10, shadowRadius:10,shadowColor:'#175E4C', shadowOffset:{width:2, height:6},shadowOpacity:0.7, backgroundColor:'#2ABB9B', padding:20}}>
         <View style={{flexDirection:'row'}}>
-          <View style={{width:80, height:80, backgroundColor:'#229E85'}}>
+          <View style={{width: 80, height: 80, backgroundColor:'#229E85', borderRadius: 40}}>
           </View>
-          <View style={{}}>
+          <View style={{alignItems: 'flex-start', marginLeft: 20, paddingTop: 10}}>
             <Text style={styles.text}>Chuck Norris</Text>
             <Text style={styles.text}>CEO</Text>
           </View>
         </View>
-        <View style={{}}>
+        <View style={{marginTop: 20}}>
           <Text style={{color:'white'}}><Icon name="ios-call-outline" size={16}/> 481-5162342</Text>
           <Text style={{color:'white'}}><Icon name="ios-send-outline" size={16}/> chuck@norrisfamily.arpa</Text>
         </View>
@@ -65,4 +65,3 @@ const styles = StyleSheet.create({
 
 ContactCard.displayName = 'ContactCard'
 export default ContactCard
-
